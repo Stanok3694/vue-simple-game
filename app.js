c555f571ad1a1edf9d70540e5cd38e3fc6e990a5
Stanislav Okunev <stanok3694@yandex.ru>
2017-04-06 17:11:43 +0300

    New array push logic
    Added new logic for pushing some information elements to log array

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
-        firstLaunch: true, 
-        stepCounter: 0,
-        allPutDamage: 0,
-        allGetDamage: 0,
+        firstLaunch: true,
+        currentState: 'begin', 
+        // stepCounter: 0,
+        // allPutDamage: 0,
+        // allGetDamage: 0,
         turns: []
     },
     methods:{
@@ -16,15 +17,13 @@ new Vue({
             this.gameIsRunning = true;
             this.playerHealth = 100,
             this.monsterHealth = 100,
-            this.firstLaunch = false,
-            this.stepCounter = 0,
-            this.allPutDamage = 0,
-            this.allGetDamage = 0,
-            this.turns = [],
-            this.turns.unshift({
-                isFirstLaunch: true,
-                text: 'Battle begins...'
-            });
+            this.firstLaunch = false
+            this.setLogElementsInTurns('begin');
+            // statistic variables:
+            // this.stepCounter = 0,
+            // this.allPutDamage = 0,
+            // this.allGetDamage = 0,
+            
         },
         attack: function () {
             this.playerAttack(3,10);
@@ -49,42 +48,25 @@ new Vue({
             } else {
                 this.playerHealth = 100;
             }
-            this.turns.unshift({
-                    isPlayer: true,
-                    isPlayerFeelGood: true,
-                    text: 'Player heals himself for 10'
-            });
+            this.setLogElementsInTurns('heal');
             this.monsterAttack(5,12);
         },
         giveUp: function () {
             this.gameIsRunning = false;
-            this.turns = [];
-            this.turns.unshift({
-                isPlayer: true,
-                isPlayerFeelGood: false,
-                text: 'No glory - no mercy! You lost your own battle!'
-            });
+            this.setLogElementsInTurns('lose');
         },
         // service section
         playerAttack: function(min, max){
             var dealDamage = this.calculateDamage(min,max);
             this.monsterHealth -= dealDamage;
-            this.allPutDamage += dealDamage;
-            this.stepCounter += 1;
-            this.turns.unshift({
-                isPlayer: true,
-                isPlayerFeelGood: true,
-                text: 'Player hits Monster for' + dealDamage
-            });
+            this.setLogElementsInTurns('player-attack', dealDamage);
+            // this.allPutDamage += dealDamage;
+            // this.stepCounter += 1;
         },
         monsterAttack: function(min,max){
             var dealDamage = this.calculateDamage(min,max);
             this.playerHealth -= dealDamage;
-            this.turns.unshift({
-                isPlayer: false,
-                isPlayerFeelGood: false,
-                text: 'Monster hits Player for' + dealDamage
-            });
+            this.setLogElementsInTurns('monster-attack', dealDamage);
         },
         calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -92,18 +74,28 @@ new Vue({
         winStatus: function() {
             if(this.monsterHealth <= 0){
                 this.gameIsRunning = false;
-                this.turns = [];
-                this.turns.unshift({
-                    isPlayer: true,
-                    isPlayerWin: true,
-                    text: 'Glory day! You won in your battle!'
-                });
+                this.setLogElementsInTurns('win');
                 return true;
             } else if (this.playerHealth <= 0){
-                this.giveUp();
+                this.gameIsRunning = false;
+                this.setLogElementsInTurns('lose');
                 return true;
             }
             return false;
+        },
+        setLogElementsInTurns: function (currentState, additionalInfo) {
+            if(currentState == 'begin') this.turns.unshift({text: 'Battle begins...'})
+            if(currentState == 'player-attack') this.turns.unshift({text: 'Player hits Monster for ' + additionalInfo})
+            if(currentState == 'monster-attack') this.turns.unshift({text: 'Monster hits Player for ' + additionalInfo})
+            if(currentState == 'heal') this.turns.unshift({text: 'Player heals himself for 10'})
+            if(currentState == 'win'){
+                this.turns = [];
+                this.turns.unshift({text: 'Glory day! You won in your battle!'});
+            }
+            if(currentState == 'lose'){
+                this.turns = [];
+                this.turns.unshift({text: 'No glory - no mercy! You lost your own battle!'})
+            }
         }
     }
 })
